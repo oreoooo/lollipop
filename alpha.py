925ebdf616f7216a9ac3dfdc98a4f3c39a799fce
oreoooo <42167590+oreoooo@users.noreply.github.com>
2018-08-23 13:48:26 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,6 @@
+char=input("enter the value")
+if (char>='a' and char<='z') or (char>='A' and char<='Z'):
+   print("the given value is aphabet")
+else:
+   print("the given value is not aalphabet")
+   
