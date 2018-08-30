4b8cca9ea16e708be4ca7d1a45f560643878b097
oreoooo <42167590+oreoooo@users.noreply.github.com>
2018-08-30 13:50:16 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,9 @@
+a=input("enter the first number")
+b=input("enter the second number")
+c=input("enter the third number")
+if (a>b)and(a>c):
+       print("a is greater among thre numbers")
+elif (b>a)and(b>c):
+       print("b is greater among three numbers")
+else:
+       print("c is greater number")
